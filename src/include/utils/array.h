541046c1ad8800cf89e9ae883a1c4338662f907a
v.pogosyan <v.pogosyan@postgresql.ru>
2023-12-24 17:28:42 +0700

    Some refactoring

--- src/include/utils/array.h
+++ src/include/utils/array.h
@@ -7,14 +7,14 @@ typedef float*** Matrix3D;
 
 Vector init_zero_vector(int size);
 Matrix init_zero_matrix(int x_size, int y_size);
-Matrix3D init_zero_matrix3Dd(int x_size, int y_size, int z_size);
+Matrix3D init_zero_matrix3d(int x_size, int y_size, int z_size);
 
 void free_vector(Vector vector);
 void free_matrix(Matrix matrix);
-void free_matrix3Dd(Matrix3D array);
+void free_matrix3d(Matrix3D array);
 
 void print_vector(Vector vector, int size);
 void print_matrix(Matrix matrix);
-void print_matrix3Dd(Matrix3D array);
+void print_matrix3d(Matrix3D array);
 
 #endif /* UTILS_H */
